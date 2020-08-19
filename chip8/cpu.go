970b4250aa52531d6ac785a1a1869024549dc40a
LGiki <qq547221432@gmail.com>
2020-08-19 22:03:42 +0800

    WIP: Implement opcode

--- chip8/cpu.go
+++ chip8/cpu.go
@@ -1,22 +1,10 @@
 package chip8
 
 type CPU struct {
-	// 8-bit general purpose register(from V0 to VF)
-	V [16]byte
-	// Address register
-	I uint16
-	// Program counter
-	PC uint16
-	// Stack pointer
-	SP byte
-	// Delay timer
-	DT byte
-	// Sound timer
-	ST byte
+	Register Register
+	Memory   Memory
 	// internal stack to store return addresses when calling procedures
 	Stack [16]uint16
-	// Memory
-	Memory Memory
 }
 
 func (cpu *CPU) Cycle(opcode uint16) {
@@ -29,37 +17,100 @@ func (cpu *CPU) Cycle(opcode uint16) {
 	//case 0x0000:
 	// 1NNN: goto NNN
 	case 0x1000:
-		cpu.PC = nnn
+		cpu.Register.PC = nnn
 	// 2NNN: Calls subroutine at NNN
 	case 0x2000:
-		cpu.SP++
-		cpu.Stack[cpu.SP] = cpu.PC
-		cpu.PC = nnn
+		cpu.Register.SP++
+		cpu.Stack[cpu.Register.SP] = cpu.Register.PC
+		cpu.Register.PC = nnn
 	// 3XNN: Skips the next instruction if VX equals NN
 	case 0x3000:
-		if cpu.V[x] == nn {
-			cpu.PC += 4
+		if cpu.Register.V[x] == nn {
+			cpu.Register.PC += 4
 		} else {
-			cpu.PC += 2
+			cpu.Register.PC += 2
 		}
 	// 4XNN: Skips the next instruction if VX doesn't equal NN
 	case 0x4000:
-		if cpu.V[x] != nn {
-			cpu.PC += 4
+		if cpu.Register.V[x] != nn {
+			cpu.Register.PC += 4
 		} else {
-			cpu.PC += 2
+			cpu.Register.PC += 2
 		}
 	// 5XY0: Skips the next instruction if VX equals VY
 	case 0x5000:
-		if cpu.V[x] == cpu.V[y] {
-			cpu.PC += 4
+		if cpu.Register.V[x] == cpu.Register.V[y] {
+			cpu.Register.PC += 4
 		} else {
-			cpu.PC += 2
+			cpu.Register.PC += 2
 		}
 	// 6XNN: Sets VX to NN
 	case 0x6000:
-		cpu.V[x] = nn
-		cpu.PC += 2
+		cpu.Register.V[x] = nn
+		cpu.Register.PC += 2
+	// 7XNN: Adds NN to VX (Carry flag is not changed)
+	case 0x7000:
+		cpu.Register.V[x] += nn
+		cpu.Register.PC += 2
+	case 0x8000:
+		switch opcode & 0x000F {
+		// 8XY0: Sets VX to the value of VY
+		case 0x0000:
+			cpu.Register.V[x] = cpu.Register.V[y]
+			cpu.Register.PC += 2
+		// 8XY1: Sets VX to VX or VY (Bitwise OR operation)
+		case 0x0001:
+			cpu.Register.V[x] |= cpu.Register.V[y]
+			cpu.Register.PC += 2
+		// 8XY2: Sets VX to VX and VY (Bitwise AND operation)
+		case 0x0002:
+			cpu.Register.V[x] &= cpu.Register.V[y]
+			cpu.Register.PC += 2
+		// 8XY3: Sets VX to VX xor VY
+		case 0x0003:
+			cpu.Register.V[x] ^= cpu.Register.V[y]
+			cpu.Register.PC += 2
+		// 8XY4: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
+		case 0x0004:
+			if cpu.Register.V[y] > (0xFF - cpu.Register.V[x]) {
+				cpu.Register.V[0xF] = 1
+			} else {
+				cpu.Register.V[0xF] = 0
+			}
+			cpu.Register.V[x] += cpu.Register.V[y]
+			cpu.Register.PC += 2
+		// 8XY5: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+		case 0x0005:
+			if cpu.Register.V[y] > cpu.Register.V[x] {
+				cpu.Register.V[0xF] = 0
+			} else {
+				cpu.Register.V[0xF] = 1
+			}
+			cpu.Register.V[x] -= cpu.Register.V[y]
+			cpu.Register.PC += 2
+		// 8XY6: Stores the least significant bit of VX in VF and then shifts VX to the right by 1.
+		case 0x0006:
+			//TODO: Complete 8XY6
+		// 8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+		case 0x0007:
+			if cpu.Register.V[x] > cpu.Register.V[y] {
+				cpu.Register.V[0xF] = 0
+			} else {
+				cpu.Register.V[0xF] = 1
+			}
+			cpu.Register.V[x] = cpu.Register.V[y] - cpu.Register.V[x]
+			cpu.Register.PC += 2
+		// 8XYE: Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
+		case 0x000E:
+			//TODO: Complete 8XYE
+		}
+	// 9XY0: Skips the next instruction if VX doesn't equal VY. (Usually the next instruction is a jump to skip a code block)
+	case 0x9000:
+		if cpu.Register.V[x] != cpu.Register.V[y] {
+			cpu.Register.PC += 4
+		} else {
+			cpu.Register.PC += 2
+		}
 	}
 
 }
---
+++ chip8/register.go
@@ -0,0 +1,16 @@
+package chip8
+
+type Register struct {
+	// 8-bit general purpose register(from V0 to VF)
+	V [16]byte
+	// Address register
+	I uint16
+	// Program counter
+	PC uint16
+	// Stack pointer
+	SP byte
+	// Delay timer
+	DT byte
+	// Sound timer
+	ST byte
+}
